7e9da22f3cd6a107bb21ea6d722758c21bf2ad15
ASSET <asset@MacBook-Pro-ASSET.local>
2024-07-14 15:03:33 +0200

    Set up server with Express and Tailwind CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Send index.html for all requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(port, () => {
