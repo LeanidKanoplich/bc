759820b0190dac626b5335b1297918f6c83d5201
ASSET <asset@MacBook-Pro-ASSET.local>
2024-07-17 12:42:49 +0200

    Add missing index.js and ensure root element in index.html

--- public/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
-    accordionHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            const body = header.nextElementSibling;
-            body.classList.toggle('hidden');
-        });
-    });
-});
